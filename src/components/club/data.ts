05e8f70a47a5491527d766d0251982dbfc40fc87
Mohammed Omar <omarofficial1054@gmail.com>
2026-09-07 15:10:16 +0530

    previous commit work directed to incorrect path, fixed it in this commit

--- src/components/club/data.ts
+++ src/components/club/data.ts
@@ -200,7 +200,7 @@ export const DEPARTMENTS: Department[] = [
         "lavanya-d",
         "+91 80730 48671",
       ),
-      photo: "members/EventManagement/lavanya-d.jpg",
+      photo: "members/EventManagers/lavanya.jpeg",
 
     },
       

4321179a6f0ed274745056652298a8667c426342
Mohammed Omar <omarofficial1054@gmail.com>
2026-09-11 17:18:27 +0530

    re-adding component/devfoliobutton.tsx content as it was left empty previous commit due to merge conflict

---
+++ src/components/DevfolioButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+
+const DEVFOLIO_SDK_ID = "devfolio-apply-sdk";
+
+export default function DevfolioButton() {
+  useEffect(() => {
+    // Prevent the SDK from being loaded multiple times.
+    if (document.getElementById(DEVFOLIO_SDK_ID)) {
+      return;
+    }
+
+    const script = document.createElement("script");
+
+    script.id = DEVFOLIO_SDK_ID;
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div
+      className="apply-button"
+      data-hackathon-slug="HACKURITY"
+      data-button-theme="dark"
+      style={{
+        height: "44px",
+        width: "312px",
+      }}
+    />
+  );
+}
